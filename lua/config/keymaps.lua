4ec546ca0940bfb44303b5d46104130faad29cd2
WorthyD <danieljworthy@gmail.com>
2023-05-10 19:53:52 -0500

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,38 @@
+-- This file is automatically loaded by lazyvim.plugins.config
+
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+map("n", ",h", "<C-w>h", { desc = "Go to left window" })
+map("n", ",j", "<C-w>j", { desc = "Go to lower window" })
+map("n", ",k", "<C-w>k", { desc = "Go to upper window" })
+map("n", ",l", "<C-w>l", { desc = "Go to right window" })
+
+
+map("i", "jj", "<Esc>")
+
+-- Tabs
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+if Util.has("neotree.nvim") then
+    map("n", "o", "<2-LeftMouse>", { desc = "Open" })
+  end
