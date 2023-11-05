05d910547fcc5788f0e9fe261eb7c3c4cf444988
WorthyD <danieljworthy@gmail.com>
2023-11-04 19:31:55 -0500

    adding color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+-- return {
+--     { "martinsione/darkplus.nvim" },
+  
+--     -- Configure LazyVim to load gruvbox
+--     {
+--       "LazyVim/LazyVim",
+--       opts = {
+--         colorscheme = "darkplus",
+--       },
+--     }
+--   }
